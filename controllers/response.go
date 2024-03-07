12a7b09f7b12d153f7eafcf7de394022eb2edbe0
Xinhao_Zhao <2272046066@qq.com>
2024-03-07 09:54:52 +0800

    添加部分注释

--- controllers/response.go
+++ controllers/response.go
@@ -1,11 +1,13 @@
 package controllers
 
+// Response 返回响应结构体
 type Response struct {
 	Code    int         `json:"code"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
 }
 
+// MakeResponse 生成一个响应
 func MakeResponse(code int, message string, data interface{}) Response {
 	return Response{
 		Code:    code,
