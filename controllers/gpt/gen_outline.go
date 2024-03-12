2fa5274ef22df7ef00c41448aa9d93ba82f79ff9
Xinhao_Zhao <2272046066@qq.com>
2024-03-12 21:46:18 +0800

    添加调试模式

--- controllers/gpt/gen_outline.go
+++ controllers/gpt/gen_outline.go
@@ -25,31 +25,42 @@ func (this *Controller) GenOutline() {
 	prompt = strings.ReplaceAll(prompt, "{{sponsor}}", request.Sponsor)
 
 	outline_str := ``
-	debug := 0 // 调试模式
+	debug := 1 // 调试模式
 	if debug == 1 {
 		// 如果调试模式开启，则使用固定的大纲字符串
 		// 粥p差不多得了
 		outline_str = `<slides>
-		<section class='cover'>
-		<p>我为什么玩明日方舟</p>
-		<p>汇报人：dhf</p>
-		</section>
-		<section
-		class='catalog'>
-		<p>目录</p>
-		<p>1. 玩法简介</p>
-		<p>2. 游戏特色</p>
-		<p>3. 社交互动</p>
-		<p>4. 个人体验</p>
-		<p>5. 结束语</p>
+    	<section class='cover'>
+        <p>我为什么玩明日方舟</p>
+        <p>汇报人：dhf</p>
+    	</section>
+    	<section class='catalog'>
+        <p>目录</p>
+        <p>1. 游戏背景</p>
+        <p>2. 独特的人设</p>
+        <p>3. 挑战性的玩法</p>
+        <p>4. 社区互动</p>
+        <p>5. 结语</p>
+    	</section>
+    	<section class='content'>
+        <p>游戏背景</p>
+        <p>内容概要：介绍明日方舟的世界观，讲述感染者与感染病毒的斗争，以及玩家在游戏中扮演的医疗救援人员的角色。</p>
+    	</section>
+    	<section class='content'>
+        <p>独特的人设</p>
+        <p>内容概要：分享明日方舟各种不同种族，不同职业的角色形象，以及他们的个性、故事和能力等。</p>
+    	</section>
+    	<section class='content'>
+        <p>挑战性的玩法</p>
+        <p>内容概要：介绍明日方舟各种游戏模式和关卡，以及它们的难度和挑战性，包括如何提高玩家的战斗技巧和策略。</p>
 		</section>
 		<section class='content'>
-		<p>社交互动</p>
-		<p>1. 分享自己在游戏中的心得体会有助于与其他玩家建立更紧密的联系，增强游戏体验。</p>
-		<p>2. 参与游戏社区的互动活动，不仅可以赢取奖励，还能结交志同道合的朋友。</p>
-		<p>3. 玩家之间的互动是游戏中不可或缺的一部分，可以互相帮助、交流游戏心得、组队挑战副本等。</p>
-		<p>4. 玩家之间的互动是游戏中不可或缺的一部分，可以互相帮助、交流游戏心得、组队挑战副本等。</p>
-		<p>5. 玩家之间的互动是游戏中不可或缺的一部分，可以互相帮助、交流游戏心得、组队挑战副本等。</p>
+        <p>社区互动</p>
+        <p>内容概要：讲述明日方舟社区的特点和优势，包括玩家之间的互动、交流和创作，以及开发团队与玩家之间的沟通和回应。</p>
+		</section>
+		<section class='content'>
+        <p>结语</p>
+        <p>内容概要：总结明日方舟的优点和吸引力，以及我个人对它的喜爱和热爱，鼓励更多的人加入明日方舟的世界。</p>
 		</section>
 		</slides>`
 	} else {
