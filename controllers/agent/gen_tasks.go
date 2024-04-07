9f63f3f5b2fda771ac12ff2136a6c800c20fda64
Xinhao_Zhao <2272046066@qq.com>
2024-04-07 15:53:14 +0800

    生成任务接口

--- controllers/agent/gen_tasks.go
+++ controllers/agent/gen_tasks.go
@@ -4,20 +4,12 @@ import (
 	"backend/conf"
 	"backend/controllers"
 	"backend/controllers/gpt"
-	"backend/models"
 	"encoding/json"
-	"encoding/xml"
 	"strings"
 )
 
-type TasksXML struct {
-	XMLName xml.Name `xml:"tasks"`
-	Task    []string `xml:"task"`
-}
-
 type GenTaskRequest struct {
 	Prompt string `json:"prompt"`
-	Slide  string `json:"slide"`
 }
 
 func (this *Controller) GenTasks() {
@@ -29,22 +21,17 @@ func (this *Controller) GenTasks() {
 	template := conf.GetTasksGeneratePromptTemplate()
 	template = strings.ReplaceAll(template, "{{prompt}}", request.Prompt)
 
-	tasksXML, err := gpt.RequestGptXml(template, TasksXML{}) // <tasks></tasks>
+	tasksStr, err := gpt.RequestGpt(template)
+
+	var tasks []string
+	err = json.Unmarshal([]byte(tasksStr), &tasks)
 	if err != nil {
 		this.Data["json"] = controllers.MakeResponse(controllers.Err, err.Error(), nil)
 		this.ServeJSON()
 		return
 	}
 
-	tasksXML = models.ReformatXML(tasksXML)
-
-	this.Data["json"] = controllers.MakeResponse(controllers.OK, "success", tasksXML)
+	this.Data["json"] = controllers.MakeResponse(controllers.OK, "success", tasks)
 	this.ServeJSON()
-
-	// 将xml格式的任务列表转换为结构体
-
-	// 执行每个任务
-
-	// 进行任务评价
 
 }
